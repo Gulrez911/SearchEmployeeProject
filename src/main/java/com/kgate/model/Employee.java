9065539240cfb018981cb44bf80173c52baff140
pritis2603 <pritis2603@gmail.com>
2018-12-11 11:33:08 +0530

    11/12/2018

--- src/main/java/com/kgate/model/Employee.java
+++ src/main/java/com/kgate/model/Employee.java
@@ -25,7 +25,6 @@ import org.hibernate.annotations.NamedQueries;
 import org.hibernate.annotations.NamedQuery;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.NotEmpty;
-
 @Entity
 @Table(name = "employee123")
 
@@ -48,7 +47,6 @@ public class Employee implements Serializable {
 
 	@Column
 	private String otp;
-
 	public String getOtp() {
 		return otp;
 	}
@@ -79,150 +77,67 @@ public class Employee implements Serializable {
 
 	@Column
 	private String telephone;
-<<<<<<< HEAD
 
 	@Column
 	private String category;
-	
-    @Column
-    private String password;
-    
-    
-=======
-
-	@Column
-	private String category;
->>>>>>> branch 'master' of https://github.com/Gulrez911/SearchEmployeeProject.git
 
 	@Column
 	private String status;
 
-<<<<<<< HEAD
-	
-=======
 	@Column
 	private String password;
->>>>>>> branch 'master' of https://github.com/Gulrez911/SearchEmployeeProject.git
 
 	@Column
-<<<<<<< HEAD
-	private int managerId;
-=======
->>>>>>> branch 'master' of https://github.com/Gulrez911/SearchEmployeeProject.git
 
-<<<<<<< HEAD
-	public int getId() {
-		return id;
-=======
 	private int managerId;
 	public String getAadhar() {
 		return aadhar;
->>>>>>> branch 'master' of https://github.com/Gulrez911/SearchEmployeeProject.git
 	}
 
-<<<<<<< HEAD
-	public void setId(int id) {
-		this.id = id;
-=======
 	public void setAadhar(String aadhar) {
 		this.aadhar = aadhar;
->>>>>>> branch 'master' of https://github.com/Gulrez911/SearchEmployeeProject.git
 	}
 
-<<<<<<< HEAD
-	public String getName() {
-		return name;
-=======
 	public String getPan() {
 		return pan;
->>>>>>> branch 'master' of https://github.com/Gulrez911/SearchEmployeeProject.git
 	}
 
-<<<<<<< HEAD
-	public void setName(String name) {
-		this.name = name;
-=======
 	public void setPan(String pan) {
 		this.pan = pan;
->>>>>>> branch 'master' of https://github.com/Gulrez911/SearchEmployeeProject.git
 	}
 
-<<<<<<< HEAD
-	public String getEmail() {
-		return email;
-	}
-=======
 	@Column
 	private String aadhar;
 	@Column
 	private String pan;
->>>>>>> branch 'master' of https://github.com/Gulrez911/SearchEmployeeProject.git
 
-<<<<<<< HEAD
-	public void setEmail(String email) {
-		this.email = email;
-=======
 	public int getId() {
 		return id;
->>>>>>> branch 'master' of https://github.com/Gulrez911/SearchEmployeeProject.git
 	}
 
-<<<<<<< HEAD
-	public String getAddress() {
-		return address;
-=======
 	public void setId(int id) {
 		this.id = id;
->>>>>>> branch 'master' of https://github.com/Gulrez911/SearchEmployeeProject.git
 	}
 
-<<<<<<< HEAD
-	public void setAddress(String address) {
-		this.address = address;
-=======
 	public String getName() {
 		return name;
->>>>>>> branch 'master' of https://github.com/Gulrez911/SearchEmployeeProject.git
 	}
 
-<<<<<<< HEAD
-	public String getTelephone() {
-		return telephone;
-=======
 	public void setName(String name) {
 		this.name = name;
->>>>>>> branch 'master' of https://github.com/Gulrez911/SearchEmployeeProject.git
 	}
-
-<<<<<<< HEAD
-	public void setTelephone(String telephone) {
-		this.telephone = telephone;
-=======
 	public String getEmail() {
 		return email;
->>>>>>> branch 'master' of https://github.com/Gulrez911/SearchEmployeeProject.git
 	}
 
-<<<<<<< HEAD
-	public List<String> getSkills() {
-		return skills;
-=======
 	public void setEmail(String email) {
 		this.email = email;
->>>>>>> branch 'master' of https://github.com/Gulrez911/SearchEmployeeProject.git
 	}
 
-<<<<<<< HEAD
-	public void setSkills(List<String> skills) {
-		this.skills = skills;
-=======
 	public String getAddress() {
 		return address;
->>>>>>> branch 'master' of https://github.com/Gulrez911/SearchEmployeeProject.git
 	}
 
-<<<<<<< HEAD
-=======
 	public void setAddress(String address) {
 		this.address = address;
 	}
@@ -234,7 +149,6 @@ public class Employee implements Serializable {
 	public void setTelephone(String telephone) {
 		this.telephone = telephone;
 	}
-
 	public List<String> getSkills() {
 		return skills;
 	}
@@ -243,7 +157,6 @@ public class Employee implements Serializable {
 		this.skills = skills;
 	}
 
->>>>>>> branch 'master' of https://github.com/Gulrez911/SearchEmployeeProject.git
 	public String getStatus() {
 		return status;
 	}
@@ -263,7 +176,6 @@ public class Employee implements Serializable {
 	public String getCategory() {
 		return category;
 	}
-
 	public void setCategory(String category) {
 		this.category = category;
 	}
@@ -284,9 +196,6 @@ public class Employee implements Serializable {
 				+ aadhar + ", pan=" + pan + "]";
 	}
 
-<<<<<<< HEAD
-=======
 	 
 
->>>>>>> branch 'master' of https://github.com/Gulrez911/SearchEmployeeProject.git
 }
