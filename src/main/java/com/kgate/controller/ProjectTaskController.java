46a260221de88f67364b7f2ffe80bde552170e05
nikita20182018 <salunkenikita2012@gmail.com>
2018-12-15 12:33:07 +0530

    change in controller of test

--- src/main/java/com/kgate/controller/ProjectTaskController.java
+++ src/main/java/com/kgate/controller/ProjectTaskController.java
@@ -2,33 +2,47 @@ package com.kgate.controller;
 
 import java.util.List;
 
-import org.jboss.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import org.springframework.web.servlet.ModelAndView;
 
 import com.kgate.model.ProjectDetails;
 import com.kgate.service.ProjectService;
 
-public class ProjectTaskController 
-{
-	private static final Logger logger = Logger.getLogger(ProjectTaskController.class);
-   @Autowired
-   ProjectService projectservice;
-   
-   @RequestMapping(value ="/createproject",method = RequestMethod.GET)
-   public ModelAndView createProject(ProjectDetails p)
-   {
-	   ModelAndView model=new ModelAndView("success");
-	   if (p.getProject_id()==0)
-	   {
-		   projectservice.createProject(p);
-	   }
-	   List<ProjectDetails> pdlist=projectservice.dispalyProjects();
-	   model.addObject("pdlist", pdlist);
-	return model;
-	   
-   }
+@Controller
+public class ProjectTaskController {
+	// private static final Logger logger =
+	// Logger.getLogger(ProjectTaskController.class);
+	@Autowired
+	ProjectService projectservice;
+	//
+	// public void setPersonService(ProjectService projectservice) {
+	// this.projectservice = projectservice;
+	// }
+
+	// @RequestMapping(value = "/cproject", method = RequestMethod.POST)
+	// public ModelAndView createProject(@ModelAttribute("projectdetails") ProjectDetails projectdetails) {
+	// // ModelAndView model = new ModelAndView();
+	// ModelAndView model = new ModelAndView("success");
+	// projectservice.createProject(projectdetails);
+	//
+	// List<ProjectDetails> pdlist = projectservice.dispalyProjects();
+	//
+	// model.addObject("projectdetails", projectdetails);
+	// model.addObject("pdlist", pdlist);
+	// // model.setViewName("DashBordManager");
+	//
+	// return model;
+	//
+	// }
+
+	// @RequestMapping(value = "/cproject", method = RequestMethod.POST)
+	// public ModelAndView success() {
+	// ModelAndView andView = new ModelAndView("success");
+	//
+	// return andView;
+	// }
 }
--- src/main/webapp/WEB-INF/pages/CreateProject.jsp
+++ src/main/webapp/WEB-INF/pages/CreateProject.jsp
@@ -1,24 +1,32 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-	pageEncoding="ISO-8859-1"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
 <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+    "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Insert title here</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>New/Edit Contact</title>
+<style>
+.error {
+	color: red;
+	font-weight: bold;
+}
+</style>
+
 </head>
-<body>
-	<center>
-		<h1>CREATE PROJECT</h1>
-	</center>
-	<div>
+<body
+	background="<%=request.getContextPath()%>/resources/images/macbook_apple.jpg">
+
+
+	<div align="center">
+		<h1 style="color: orangered">Create Project</h1>
 
 		<form:form action="cproject" method="post"
 			modelAttribute="projectdetails" commandName="projectdetails">
-			<table align="cproject">
-
+			<table>
 				<form:hidden path="project_id" />
 				<tr>
 					<td>Project Name:</td>
@@ -39,41 +47,13 @@
 					<td><form:input type="Date" path="pEnd_Date" id="endDate"
 							name="endDate" /></td>
 				</tr>
+
 				<tr>
 					<td><input type="submit" value="Create Project"></td>
 
 				</tr>
-
-
 			</table>
 		</form:form>
-	</div>
-	<div>
-		<div align="right" style="size: 50%">
-			<h1>Right</h1>
-		</div>
-
-		<div align="left" style="size: 50%">
-			<h1>left</h1>
-			<form action="showproject" commandName="projectdetails">
-				<table border="1">
-					<th>PROJECTS</th>
-					<c:forEach var="projectdetails" items="${pdlist}">
-						<tr>
-							<td><a
-								href="showproject?name=${projectdetails.project_Name}"
-								style="color: greenyellow">${projectdetails.project_Name}</a></td>
-						</tr>
-					</c:forEach>
-				</table>
-			</form>
-
-		</div>
-
-
-
-
-
 
 
 	</div>
