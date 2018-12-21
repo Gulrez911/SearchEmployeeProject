66755add2edbb29fd2e29282026b5e36cb0469cd
Gulrez Farooqui <gulfarooqui1@gmail.com>
2018-12-21 21:20:56 +0530

    new file added
    Employee fetch list

--- src/main/java/com/kgate/dao/EmployeeDAOImpl.java
+++ src/main/java/com/kgate/dao/EmployeeDAOImpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.kgate.model.Employee;
+import com.kgate.model.Skill;
 import java.util.ArrayList;
 import org.hibernate.SQLQuery;
 
